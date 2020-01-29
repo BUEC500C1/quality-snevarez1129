742eac435200e3eba8a968be2a335b255499633e
Se <sebastiannevarez@Sebs-MacBook-Pro.local>
2020-01-29 14:39:20 -0500

    changed converter function in test file

--- test_convert.py
+++ test_convert.py
@@ -1,6 +1,7 @@
 def romanNum(x):
     #print("Roman!")
 
+    roma = ""
     roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"] #list of roman numerals
     arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000] #arabic value of roman numeral
     i = 12 #number of values in the above arrays
@@ -12,12 +13,13 @@ def romanNum(x):
         #print(x)
 
         while div > 0: #while the quotient is greater than 0
-            print(roman[i], end = "") #print roman numeral digit
+            roma = roma + roman[i]
+            #print(roman[i], end = "") #print roman numeral digit
             div -= 1 #decrement the quotient
 
         i -= 1 #move to the next roman numeral digit
 
-    return
+    return roma
 
 def test_conversion():
     assert romanNum(3) == "III"
