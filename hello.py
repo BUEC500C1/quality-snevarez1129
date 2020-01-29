154ccccfadbe3e40f7798a9aedd7ee7cc1106125
Se <sebastiannevarez@Sebs-MacBook-Pro.local>
2020-01-29 14:38:02 -0500

    made changes to converter

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ print("Hello, welcome to my converter!")
 def romanNum(x):
     #print("Roman!")
 
+    roma = ""
     roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"] #list of roman numerals
     arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000] #arabic value of roman numeral
     i = 12 #number of values in the above arrays
@@ -14,12 +15,13 @@ def romanNum(x):
         #print(x)
 
         while div > 0: #while the quotient is greater than 0
-            print(roman[i], end = "") #print roman numeral digit
+            roma = roma + roman[i]
+            #print(roman[i], end = "") #print roman numeral digit
             div -= 1 #decrement the quotient
 
         i -= 1 #move to the next roman numeral digit
 
-    return
+    return roma
 
 def main():
 
@@ -30,12 +32,12 @@ def main():
         #print("The Arabic Number = ", arab)
         #print("The type is = ", type(arab))
 
-        print("The Roman Numeral for", arab, "is: ", end = "")
-        romanNum(arab) #call conversion function
+        numeral = romanNum(arab) #call conversion function
+        print("The Roman Numeral for", arab, "is: ", numeral)
         #print("The Roman Numeral = ", roma) #print the converted roman numeral
         #print("The type is = ", type(roma))
 
-        cont = input("\nWould you like to enter a new number? [y/n]: ")
+        cont = input("nWould you like to enter a new number? [y/n]: ")
     
     print("Thanks for playing! Goodbye!") #exit program
 
